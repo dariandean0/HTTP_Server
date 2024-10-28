a760c8f92df6f3e5aac11feb2bdae1b443ec16c3
Darian Dean <darian.aidan@gmail.com>
2024-10-28 13:38:11 -0400

    ServerSocket error handling

--- HTTP_Server.java
+++ HTTP_Server.java
@@ -12,25 +12,19 @@ import java.net.Socket;
 
 public class HTTP_Server {
     public static void main(String[] args) throws Exception {
-        final ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080 ....");
-        
-        /*while(true) {
-            try (Socket socket = server.accept()) {
-                Date today = new Date();
-                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
-                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
-            }
+        try (ServerSocket server = new ServerSocket(8080)) {
+            System.out.println("Listening for connection on port 8080 ....");
 
-        }*/
-
-        while(true) {
-            try {
-                Socket socket = server.accept();
-                new Thread(new ClientHandler(socket)).start();
-            } catch (IOException e) {
-                System.out.println("Error accepting client: " + e.getMessage());
+            while(true) {
+                try {
+                    Socket socket = server.accept();
+                    new Thread(new ClientHandler(socket)).start();
+                } catch (IOException e) {
+                    System.out.println("Error accepting client: " + e.getMessage());
+                }
             }
+        } catch (IOException e) {
+            System.out.println("Could not start server: " + e.getMessage());
         }
 
     }
